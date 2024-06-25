5436ef9620c42008915d766abdc89382fb098784
Gabriel Ribeiro Carneiro <32889179+gabrielgrc@users.noreply.github.com>
2024-06-25 10:29:00 -0300

    Branch sums algorithm implemented

---
+++ branch_sums.py
@@ -0,0 +1,36 @@
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+#Complexity Analysis : O(n) time | O(n) space
+def branchSums(root):
+    sums = []
+    calculateBranchSums(root, 0, sums)
+    return sums
+
+def calculateBranchSums(node, runningSum, sums):
+    if node is None:
+        return
+
+    newRunningSum = runningSum + node.value
+    if node.left is None and node.right is None:
+        sums.append(newRunningSum)
+        return
+
+    calculateBranchSums(node.left, newRunningSum, sums)
+    calculateBranchSums(node.right, newRunningSum, sums)
+
+# Define a simple binary tree for testing
+root = BinaryTree(1)
+root.left = BinaryTree(2)
+root.right = BinaryTree(3)
+root.left.left = BinaryTree(4)
+root.left.right = BinaryTree(5)
+root.right.left = BinaryTree(6)
+root.right.right = BinaryTree(7)
+
+# Call the branchSums function and print the result
+sums = branchSums(root)
+print(sums)  
