379f02654cb48194e69979f4892f41ff6176f6d3
Gabriel Ribeiro Carneiro <32889179+gabrielgrc@users.noreply.github.com>
2024-07-26 13:41:49 -0300

    Middle Node algorithm implemented

---
+++ middle_node.py
@@ -0,0 +1,48 @@
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Solution:
+    def middleNode(self, linkedList):
+        slow = linkedList
+        fast = linkedList
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        return slow
+
+# Helper function to create a linked list from a list and return the head
+def createLinkedList(linkedList):
+    if not linkedList:
+        return None
+    head = LinkedList(linkedList[0])
+    currentNode = head
+    for value in linkedList[1:]:
+        currentNode.next = LinkedList(value)
+        currentNode = currentNode.next
+    return head
+
+# Helper function to print the linked list from a given node
+def printLinkedList(linkedList):
+    currentNode = linkedList
+    while currentNode:
+        print(currentNode.value, end=" -> ")
+        currentNode = currentNode.next
+    print("None")
+
+# Create a linked list from a list
+linkedListValues = [2, 7, 3, 5]
+head = createLinkedList(linkedListValues)
+
+# Find and print the middle node
+solution = Solution()
+middleNode = solution.middleNode(head)
+
+# Print the result
+print("The middle node value is :", middleNode.value)
+print("The linked list starting from the middle node is :")
+printLinkedList(middleNode)
